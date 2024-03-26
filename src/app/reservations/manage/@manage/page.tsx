3831a18018aa0557c977cf95d8be8d188246e901
Kanit <6633001921@student.chula.ac.th>
2024-03-26 20:47:00 +0700

    last auth

--- src/app/reservations/manage/@manage/page.tsx
+++ src/app/reservations/manage/@manage/page.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth"
-import { authOptions } from "@/app/api/auth/[...nextauth]/route"
+import { authOptions } from "@/libs/auth"
 import getReservations from "@/libs/getReservations"
 import { ReservationItem } from "interfaces"
 import { Button } from "@mui/material"
