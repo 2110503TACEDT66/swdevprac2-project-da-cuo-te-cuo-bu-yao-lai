894ac3b976fe10cf3482718c400dc1ca6f060c1f
Kanit <6633001921@student.chula.ac.th>
2024-03-26 22:09:01 +0700

    final final final

--- src/libs/getReservations.tsx
+++ src/libs/getReservations.tsx
@@ -1,6 +1,6 @@
 export default async function getReservations(token: string) {
-
-    const response = await fetch(`${process.env.BACKEND_URL}/api/v1/reservations`, {
+    console.log(`${process.env.BACKEND_URL}`);
+    const response = await fetch('https://presentation-day-1-da-cuo-te-cuo-bu-yao-lai.vercel.app/api/v1/reservations', {
         method: "GET",
         headers: {
             authorization: `Bearer ${token}`
